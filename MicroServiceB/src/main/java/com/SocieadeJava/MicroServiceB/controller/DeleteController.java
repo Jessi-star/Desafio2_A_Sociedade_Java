c9d3087d9d7ca79a377e3eaa591a9be8c191eef6
Natzaum <natacezerbordignon@gmail.com>
2024-11-26 17:17:17 -0300

    feat: tratando exceções

--- MicroServiceB/src/main/java/com/SocieadeJava/MicroServiceB/controller/DeleteController.java
+++ MicroServiceB/src/main/java/com/SocieadeJava/MicroServiceB/controller/DeleteController.java
@@ -1,12 +1,13 @@
 package com.SocieadeJava.MicroServiceB.controller;
 
+import com.SocieadeJava.MicroServiceB.exceptions.ResourceInUseException;
+import com.SocieadeJava.MicroServiceB.exceptions.ResourceNotFoundException;
+import com.SocieadeJava.MicroServiceB.service.CommentService;
 import com.SocieadeJava.MicroServiceB.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/posts")
@@ -15,9 +16,22 @@ public class DeleteController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private CommentService commentService;
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarPost(@PathVariable Long id){
         postService.deletePost(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ResourceInUseException.class)
+    public ResponseEntity<String> handleResourceInUse(ResourceInUseException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 }
--- MicroServiceB/src/main/java/com/SocieadeJava/MicroServiceB/service/PostService.java
+++ MicroServiceB/src/main/java/com/SocieadeJava/MicroServiceB/service/PostService.java
@@ -1,10 +1,12 @@
 package com.SocieadeJava.MicroServiceB.service;
 
 import com.SocieadeJava.MicroServiceB.entity.Post;
+import com.SocieadeJava.MicroServiceB.exceptions.ResourceInUseException;
 import com.SocieadeJava.MicroServiceB.exceptions.ResourceNotFoundException;
 import com.SocieadeJava.MicroServiceB.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,10 +35,14 @@ public class PostService {
     }
 
     public void deletePost(Long id) {
-        if(postRepository.existsById(id)){
-            postRepository.deleteById(id);
-        } else {
-            throw new ResourceNotFoundException("Post não encontrado com ID: " + id);
+        try {
+            if (postRepository.existsById(id)) {
+                postRepository.deleteById(id);
+            } else {
+                throw new ResourceNotFoundException("Post não encontrado com ID: " + id);
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new ResourceInUseException("O post " + id + " está  referenciado em outra parte do sistema.");
         }
     }
 }
