7e147d637f6d235fda79f343daa50e6c519d8427
Natzaum <natacezerbordignon@gmail.com>
2024-11-28 16:28:21 -0300

    feat: criação do metodo POST nas classes comment

--- MicroServiceB/src/main/java/com/SocieadeJava/MicroServiceB/service/CommentService.java
+++ MicroServiceB/src/main/java/com/SocieadeJava/MicroServiceB/service/CommentService.java
@@ -1,7 +1,11 @@
 package com.SocieadeJava.MicroServiceB.service;
 
+import com.SocieadeJava.MicroServiceB.dto.CommentDTO;
 import com.SocieadeJava.MicroServiceB.entity.Comment;
+import com.SocieadeJava.MicroServiceB.entity.Post;
+import com.SocieadeJava.MicroServiceB.exceptions.ResourceNotFoundException;
 import com.SocieadeJava.MicroServiceB.repository.CommentRepository;
+import com.SocieadeJava.MicroServiceB.repository.PostRepository;
 import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,25 @@ import java.util.List;
 
 @Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
 
+    @Autowired
+    public CommentService(CommentRepository commentRepository, PostRepository postRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
+    public CommentDTO createComment(Long postId, CommentDTO commentDTO){
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post não encontrado com ID: " + postId));
+
+        Comment comment = new Comment();
+        comment.setTexto(commentDTO.getBody());
+        comment.setPost(post);
+
+        comment = commentRepository.save(comment);
+
+        return CommentDTO.fromEntity(comment);
+    }
 
 }
