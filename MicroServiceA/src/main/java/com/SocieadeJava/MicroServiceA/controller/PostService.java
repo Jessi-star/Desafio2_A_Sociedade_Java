04820e8fcdac4f16c63a9e0919db1593e6c9620f
Felipe <lipem61@gmail.com>
2024-11-28 14:21:19 -0300

    feat open feign configurado

--- MicroServiceA/src/main/java/com/SocieadeJava/MicroServiceA/controller/PostService.java
+++
@@ -1,5 +0,0 @@
-package com.SocieadeJava.MicroServiceA.controller;
-
-public class PostService {
-
-}
---
+++ MicroServiceA/src/main/java/com/SocieadeJava/MicroServiceA/service/PostService.java
@@ -0,0 +1,27 @@
+package com.SocieadeJava.MicroServiceA.service;
+
+
+import com.SocieadeJava.MicroServiceA.dto.PostDTO;
+import com.SocieadeJava.MicroServiceA.intraclient.PostClient;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    private final PostClient postClient;
+
+    public PostService(PostClient postClient) {
+        this.postClient = postClient;
+    }
+
+    public List<PostDTO> fetchAllPosts() {
+        return postClient.getAllPosts();
+    }
+
+    public PostDTO createPost(PostDTO postDTO) {
+        return postClient.createPost(postDTO);
+    }
+}
