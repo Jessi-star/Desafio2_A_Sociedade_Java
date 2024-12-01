320f837d3274e142049e67dbfba85cd37ea21c9c
Emily L. Balestrin <emilyl.balestrin@gmail.com>
2024-12-01 14:34:51 -0300

    test: adicionando teste para criação de post com sucesso no PostController

--- MicroServiceA/src/test/java/com/SocieadeJava/MicroServiceA/controller/PostControllerTest.java
+++ MicroServiceA/src/test/java/com/SocieadeJava/MicroServiceA/controller/PostControllerTest.java
@@ -8,7 +8,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PostControllerTest {
@@ -24,6 +29,19 @@ public class PostControllerTest {
     @BeforeEach
     void config(){
         postDTO = new PostDTO(1L, "Teste", "Testando teste");
+    }
+
+    @Test
+    void testarCriarPost_deveRetornarPostCriado() {
+        when(postService.createPost(postDTO)).thenReturn(postDTO);
+
+        PostDTO resposta = postController.createPost(postDTO);
+
+        assertNotNull(resposta, "A resposta não deve ser nula");
+        assertEquals(postDTO.getTitle(), resposta.getTitle(), "Os títulos devem ser iguais");
+        assertEquals(postDTO.getBody(), resposta.getBody(), "Os corpos devem ser iguais");
+
+        verify(postService, times(1)).createPost(postDTO);
     }
 
     
