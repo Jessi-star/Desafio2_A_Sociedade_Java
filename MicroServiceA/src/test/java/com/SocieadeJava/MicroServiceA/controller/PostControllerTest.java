b897bc40d56a5fdb190f8ef2e464380a8ff1ef47
Emily L. Balestrin <emilyl.balestrin@gmail.com>
2024-12-01 14:15:16 -0300

    feat: adicionando configuração inicial da classe PostControllerTest

--- MicroServiceA/src/test/java/com/SocieadeJava/MicroServiceA/controller/PostControllerTest.java
+++ MicroServiceA/src/test/java/com/SocieadeJava/MicroServiceA/controller/PostControllerTest.java
@@ -1,4 +1,30 @@
 package com.SocieadeJava.MicroServiceA.controller;
 
+import com.SocieadeJava.MicroServiceA.dto.PostDTO;
+import com.SocieadeJava.MicroServiceA.service.PostService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class PostControllerTest {
+
+    @Mock
+    private PostService postService;
+
+    @InjectMocks
+    private PostController postController;
+
+    private PostDTO postDTO;
+
+    @BeforeEach
+    void config(){
+        postDTO = new PostDTO(1L, "Teste", "Testando teste");
+    }
+
+    
 }
