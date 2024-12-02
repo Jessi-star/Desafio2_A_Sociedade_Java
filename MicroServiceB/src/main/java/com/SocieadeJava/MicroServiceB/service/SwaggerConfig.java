a94e4fe6cd561df4960ce6fdde464cffd9c3c25f
Vítor Nodari <vitornodari6@gmail.com>
2024-12-02 17:08:15 -0300

    Ultima alteração, referente documentação swagger

--- MicroServiceB/src/main/java/com/SocieadeJava/MicroServiceB/service/SwaggerConfig.java
+++ MicroServiceB/src/main/java/com/SocieadeJava/MicroServiceB/service/SwaggerConfig.java
@@ -2,17 +2,18 @@ package com.SocieadeJava.MicroServiceB.service;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    public OpenAPI customOpenAPI(){
-
+    @Bean
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Api documentation")
-                        .description("Swagger UI and Springdoc OpenAPI"));
-
+                        .title("API Documentation")
+                        .description("Swagger UI and Springdoc OpenAPI for MicroService B")
+                        .version("1.0.0"));
     }
 }
